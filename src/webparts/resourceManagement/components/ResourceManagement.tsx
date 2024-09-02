5362bbb68f3cddc4732578a621880a11be3f6138
Anders Ravn Adriansen <anders.ravn.adriansen@ngage.dk>
2024-09-02 09:26:47 +0200

    initial

---
+++ src/webparts/resourceManagement/components/ResourceManagement.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface IResourceManagementProps {
+  isDarkTheme: boolean;
+  environmentMessage: string;
+}
+
+const ResourceManagement: React.FC<IResourceManagementProps> = () => {
+  return <></>
+}
+
+export default ResourceManagement;

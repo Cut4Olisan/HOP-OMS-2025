95eefde93c106fde9fc909045b63cbd7a9f19cf7
Anders Ravn Adriansen <anders.ravn.adriansen@ngage.dk>
2024-11-08 11:55:00 +0100

    Moved styles to scss file

--- src/webparts/resourceManagement/components/generic/Card.tsx
+++ src/webparts/resourceManagement/components/generic/Card.tsx
@@ -1,31 +1,11 @@
 import * as React from "react";
-
+import styles from "./Card.module.scss";
 export interface ICardProps extends React.HTMLProps<HTMLDivElement> {}
 
-const styles: React.CSSProperties = {
-  backgroundColor: "white",
-  border: "1px solid #edebe9",
-  borderRadius: 4,
-  padding: "8px 12px",
-  display: "flex",
-  flexDirection: "column",
-  gap: 8,
-  overflowY: "auto",
-  boxSizing: "border-box",
-};
-
-const Card: React.FC<React.PropsWithChildren<ICardProps>> = ({
-  style,
-  children,
-  ref,
-}) => {
-  React.useEffect(() => {
-    console.log(ref);
-  }, []);
-
+const Card: React.FC<React.PropsWithChildren<ICardProps>> = (props) => {
   return (
-    <div ref={ref} style={{ ...style, ...styles }}>
-      {children}
+    <div className={styles.card} style={props.style}>
+      {props.children}
     </div>
   );
 };
